ab5ab2abaf0bcd3e52d0b2ac79a0de4497f5656c
Hamzaoaz <67586766+Hamzaoaz@users.noreply.github.com>
2025-01-19 15:59:53 +0400

    Update ArabicIndex.tsx

--- src/pages/ArabicIndex.tsx
+++ src/pages/ArabicIndex.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Helmet } from "react-helmet";
 import { ArabicHeroSection } from "@/components/ArabicHeroSection";
 import { ArabicServicesSection } from "@/components/ArabicServicesSection";
@@ -5,19 +6,7 @@ import { ArabicBiographySection } from "@/components/ArabicBiographySection";
 import { ArabicBookingSection } from "@/components/ArabicBookingSection";
 import { ArabicSurgeriesSection } from "@/components/ArabicSurgeriesSection";
 import { NavigationIndex } from "@/components/NavigationIndex";
-import React from "react";
-import { Link } from "react-router-dom";
 
-const ArabicIndex = () => {
-  return (
-    <div>
-      <h1>مرحبًا بكم في صفحة الدكتور أسامة أبو زيد</h1>
-      <Link to="/ar/about">تعرف على المزيد عن الدكتور أسامة</Link>
-    </div>
-  );
-};
-
-export default ArabicIndex;
 const ArabicIndex = () => {
   return (
     <>
@@ -41,3 +30,4 @@ const ArabicIndex = () => {
 };
 
 export default ArabicIndex;
+
